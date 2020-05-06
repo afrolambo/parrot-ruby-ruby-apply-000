80995e2d6f5e04984df3e340e852e74c348ee577
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-06 00:45:10 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,4 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
+require_relative './spec_helper'
+require_relative './parrot_spec'
 # returns the phrase
 def parrot(phrase = "Pretty bird!", phrase = "Squawk!")
   puts "Squawk!"
